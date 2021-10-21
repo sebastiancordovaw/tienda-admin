7241973f6d76f96044276e1532cd2c0df47e22fa
sebastian cordova <sebastiancordovaw@gmail.com>
2021-10-21 15:47:20 -0300

    se agrega ngModel al formulario e izitoast

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var jQuery:any;
+declare var $:any;
+declare var iziToast:any;
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public user : any = {};
+  constructor() { 
+      
+    $('.cs-offcanvas-enabled').css({'padding-left':'0px','display':'block'});
+    $('.cs-offcanvas-enabled > .col-xl-9').css({'max-width':'100%'});
+  }
 
   ngOnInit(): void {
   }
 
+  login(loginForm: any){
+    
+    if(loginForm.valid)
+    {
+      console.log(this.user);
+      alert("valido")
+    }
+    else
+    {
+      iziToast.error({
+        title:'Error',
+        class:'text-danger',
+        position:'topCenter',
+        message:'los datos no son validos'
+      })
+    }
+  }
 }
