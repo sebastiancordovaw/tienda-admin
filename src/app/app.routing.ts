9462d34d12bb144a7e0f08dea9c1d9449cf8454c
sebastian cordova <sebastiancordovaw@gmail.com>
2021-10-25 17:49:27 -0300

    se agrega las rutas al routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,7 +6,7 @@ import { IndexClienteComponent } from "./components/clientes/index-cliente/index
 import { AdminGuard } from "./guards/admin.guard";// para proteger rutas
 
 const appRoute : Routes = [
-    {path:'',component:InicioComponent, canActivate:[AdminGuard]},
+    {path:'inicio',component:InicioComponent, canActivate:[AdminGuard]},
     {path:'panel', children:[
         {path:'clientes', component:IndexClienteComponent, canActivate:[AdminGuard]}
     ]},
